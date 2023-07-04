309964dbb36835909cf4470817088e6c9f264117
Kim Joohee <syap1170@gmail.com>
2023-07-04 17:46:20 +0900

    docs: 회원가입 트랜젝션

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { Users, UserInfos } = require('../models');
+const { Users, UserInfos, sequelize } = require('../models');
 const router = express.Router();
+const { Transaction } = require('sequelize');
 
 // 회원가입
 router.post('/users', async (req, res) => {
@@ -12,16 +13,35 @@ router.post('/users', async (req, res) => {
     return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
   }
 
-  // Users 테이블에 사용자를 추가합니다.
-  const user = await Users.create({ email, password });
-  // UserInfos 테이블에 사용자 정보를 추가합니다.
-  const userInfo = await UserInfos.create({
-    UserId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
-    name,
-    age,
-    gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
-    profileImage,
+  // 1. 트랜젝션 객체를 할당
+  const transaction = await sequelize.transaction({
+    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED, // 격리 수준을 READ_COMMITTED로 설정
   });
+
+  try {
+    // Users 테이블에 사용자를 추가합니다.
+    const user = await Users.create({ email, password }, { transaction });
+    // UserInfos 테이블에 사용자 정보를 추가합니다.
+    const userInfo = await UserInfos.create(
+      {
+        UserId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
+        name,
+        age,
+        gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
+        profileImage,
+      },
+      { transaction },
+    );
+
+    // 모든 로직이 완료된 경우 Commit
+    await transaction.commit();
+  } catch (transactionError) {
+    // 트랜젝션 내에서 작업이 실패한 경우 : DB에 트랜젝션 내의 작업 내역을 취소
+    await transaction.rollback();
+    return res
+      .status(400)
+      .json({ errorMessage: '유저 생성에 실패하였습니다.' });
+  }
 
   return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
 });
